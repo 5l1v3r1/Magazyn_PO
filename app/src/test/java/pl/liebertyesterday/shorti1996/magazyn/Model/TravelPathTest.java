fbc6567db37025847743f9b7304dcbc8d0ec4c9b
Wojtek Li <shorti1996@gmail.com>
2018-01-14 03:31:52 +0100

    2pu parametrized test TravelPathTest

--- app/src/test/java/pl/liebertyesterday/shorti1996/magazyn/Model/TravelPathTest.java
+++ app/src/test/java/pl/liebertyesterday/shorti1996/magazyn/Model/TravelPathTest.java
@@ -1,77 +1,81 @@
 package pl.liebertyesterday.shorti1996.magazyn.Model;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 
 import pl.liebertyesterday.shorti1996.magazyn.AnnealingSimulator;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by wojciech.liebert on 13.01.2018.
  */
+@RunWith(Parameterized.class)
 public class TravelPathTest {
 
-    @Test
-    public void t() {
-//        LinkedList<Lokalizacja> lokalizacje = new LinkedList<Lokalizacja>() {{
-//            add(new Lokalizacja(8, 1));
-//            add(new Lokalizacja(2, 5));
-//            add(new Lokalizacja(2, 3));
-//            add(new Lokalizacja(7, 20));
-//        }};
-//        TravelPath travelPath = new TravelPath(lokalizacje);
-//        Observable.just(new AnnealingSimulator(travelPath))
-////                .subscribeOn(Schedulers.computation())
-//                .subscribe(annealingSimulator ->
-//                        annealingSimulator.simulateAnnealing(100000, 1000000000, 11111.8));
+    static final double DELTA = 1e-5;
 
-        LinkedList<Lokalizacja> lokalizacje = new LinkedList<Lokalizacja>() {{
-            add(new Lokalizacja(1, 1));
-            add(new Lokalizacja(8, 1));
-            add(new Lokalizacja(2, 5));
-            add(new Lokalizacja(3, 5));
-            add(new Lokalizacja(4, 5));
-            add(new Lokalizacja(6, 5));
-            add(new Lokalizacja(3, 25));
-            add(new Lokalizacja(3, 28));
-            add(new Lokalizacja(7, 20));
-            add(new Lokalizacja(8, 3));
-        }};
+    private final List<Lokalizacja> mLokalizacje;
+    private final List<Lokalizacja> mExpectedLokalizacje;
 
-//        lokalizacje = new LinkedList<Lokalizacja>() {{
-//            add(new Lokalizacja(1, 1));
-//            add(new Lokalizacja(3, 1));
-//            add(new Lokalizacja(8, 22));
-//            add(new Lokalizacja(5, 5));
-//            add(new Lokalizacja(8, 29));
-//            add(new Lokalizacja(8, 1));
-//        }};
-
-        lokalizacje = new LinkedList<Lokalizacja>() {{
-            add(new Lokalizacja(1, 1));
-            add(new Lokalizacja(7, 3));
-            add(new Lokalizacja(5, 3));
-            add(new Lokalizacja(2, 5));
-            add(new Lokalizacja(8, 1));
-        }};
-
-        lokalizacje = new LinkedList<Lokalizacja>() {{
-            add(new Lokalizacja(1, 1));
-            add(new Lokalizacja(7, 3));
-            add(new Lokalizacja(5, 3));
-            add(new Lokalizacja(2, 55));
-            add(new Lokalizacja(8, 1));
-        }};
-
-
-        double a = new AnnealingSimulator(new TravelPath(lokalizacje)).
-                simulateAnnealingForDistance(100, 0.99999);
-//        double b = new AnnealingSimulator(new TravelPath(lokalizacje)).
-//                simulateAnnealingForDistance(1000000000, 0.5);
-//        double c = new AnnealingSimulator(new TravelPath(lokalizacje)).
-//                simulateAnnealingForDistance(5, 0.9999);
-//        double d = new AnnealingSimulator(new TravelPath(lokalizacje)).
-//                simulateAnnealingForDistance(20, 100000000, 0.1);
-        int i = 0;
+    public TravelPathTest(List<Lokalizacja> lokalizacje, List<Lokalizacja> expected) {
+        mLokalizacje = lokalizacje;
+        mExpectedLokalizacje = expected;
     }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new LinkedList<Lokalizacja>() {{
+                    add(new Lokalizacja(1, 1));
+                    add(new Lokalizacja(7, 3));
+                    add(new Lokalizacja(5, 3));
+                    add(new Lokalizacja(2, 55));
+                    add(new Lokalizacja(8, 1));
+                }}, new LinkedList<Lokalizacja>() {{
+                    add(new Lokalizacja(1, 1));
+                    add(new Lokalizacja(2, 55));
+                    add(new Lokalizacja(5, 3));
+                    add(new Lokalizacja(7, 3));
+                    add(new Lokalizacja(8, 1));
+                }}},
+                {new LinkedList<Lokalizacja>() {{
+                    add(new Lokalizacja(1, 1));
+                    add(new Lokalizacja(8, 1));
+                    add(new Lokalizacja(2, 5));
+                    add(new Lokalizacja(3, 5));
+                    add(new Lokalizacja(4, 5));
+                    add(new Lokalizacja(6, 5));
+                    add(new Lokalizacja(3, 25));
+                    add(new Lokalizacja(3, 28));
+                    add(new Lokalizacja(7, 20));
+                    add(new Lokalizacja(8, 3));
+                }}, new LinkedList<Lokalizacja>() {{
+                    add(new Lokalizacja(1, 1));
+                    add(new Lokalizacja(2, 5));
+                    add(new Lokalizacja(3, 28));
+                    add(new Lokalizacja(3, 25));
+                    add(new Lokalizacja(3, 5));
+                    add(new Lokalizacja(4, 5));
+                    add(new Lokalizacja(6, 5));
+                    add(new Lokalizacja(7, 20));
+                    add(new Lokalizacja(8, 1));
+                    add(new Lokalizacja(8, 3));
+                }}}
+        });
+    }
+
+    @Test
+    public void test() {
+        double distance = new AnnealingSimulator(new TravelPath(mLokalizacje))
+                .simulateAnnealingForDistance(100, 0.99999);
+        assertEquals(new TravelPath(mExpectedLokalizacje).getDistance(), distance, DELTA);
+    }
+
 }
